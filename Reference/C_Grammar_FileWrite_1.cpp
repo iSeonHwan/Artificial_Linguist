9a397e1af0118d275715284bea23605d313e8079
iseonhwan <bollokrenzhaeanseon@gmail.com>
2021-10-02 22:32:12 +0900

    C의 파일 쓰기 코드를 조금 조사하였다. C 컴파일의 업데이트로 문법에 사소한 변경이 있는 것으로 보인다.

--- Reference/C_Grammar_FileWrite_1.cpp
+++ Reference/C_Grammar_FileWrite_1.cpp
@@ -1,7 +1,6 @@
 /*
 Define: 파일을 쓰는 C 함수 fopen_s()에 대한 간단한 예시
 Write datd: 2021.10.1.
-Resource: 공대누나의 일상과 전자공학. “[C언어 독학하기] 21. 파일 입출력, puts fgetc, fputc”, 2020년 12월 31일. https://gdnn.tistory.com/110?category=886154.
 
 */
 
@@ -11,7 +10,9 @@ Resource: 공대누나의 일상과 전자공학. “[C언어 독학하기] 21. 
 int main(){
 	FILE * fp;
 	int end = 0;
-	fopen_s(&fp, "text_name.txt", "w"); //우분투 환경에서 컴파일 오류가 발생하였다. 빌드 환경에 따라 문법에 차이가 있는 것 같다.
+	//fopen_s(&fp, "text_name.txt", "w"); //우분투 환경에서 컴파일 오류가 발생하였다. 빌드 환경에 따라 문법에 차이가 있는 것 같다.
+
+	fp = fopen("test.txt", "w"); //“C 언어 코딩 도장: 70.3 파일에 문자열 쓰기”에 의하면, "#define _CRT_SECURE_NO_WARNINGS"가 필요하다. 최신 C 컴파일러에서는 보안상 내용이 바뀐 것으로 보인다.
 
 	if (fp == NULL){
 		printf("실패\n");
@@ -27,3 +28,10 @@ int main(){
 	fclose(fp);
 	return 0;
 }
+
+/*
+Resource: 
+공대누나의 일상과 전자공학. “[C언어 독학하기] 21. 파일 입출력, puts fgetc, fputc”, 2020년 12월 31일. https://gdnn.tistory.com/110?category=886154.
+코딩도장. “C 언어 코딩 도장: 70.3 파일에 문자열 쓰기”. 접근된 2021년 10월 2일. https://dojang.io/mod/page/view.php?id=609.
+
+*/

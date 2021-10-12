924bec7ad0c1422b39f5e43059d332d271aa3524
iseonhwan <bollokrenzhaeanseon@gmail.com>
2021-10-12 23:57:05 +0900

    HPSG 미트릭스를 조금 추가하였다. 좀 더 구체적이고 정확한 내용을 찾아서 보충해야 되겠다.

---
+++ Reference/C_DItem.h
@@ -0,0 +1,26 @@
+/*
+Define: Dictionary frame
+Author: iseonhwan
+Date: 2021.10.12.
+*/
+
+#ifndef DITEM
+#define DITEM
+
+struct DItem{
+	//entry is Name of Dictionary word, morpheme etc. if sound message, How? Think a later.
+	char entry;
+	//Meaning of syntax.
+	//Information about the relation of cooccurence.
+	//Information about the relation of replacement.
+	
+	//Referential meaning
+	
+	//Connotative meaning
+	
+	//Contextual meaning 
+
+};
+
+
+#endif DITEM

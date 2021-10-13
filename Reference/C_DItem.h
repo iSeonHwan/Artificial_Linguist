7558ad6f56ca1cbde3c9b0983394ddf3bfcd6540
iseonhwan <bollokrenzhaeanseon@gmail.com>
2021-10-13 23:54:59 +0900

    HPSG 자질에 대해 조금 더 생각하였다.

--- Reference/C_DItem.h
+++ Reference/C_DItem.h
@@ -10,9 +10,20 @@ Date: 2021.10.12.
 struct DItem{
 	//entry is Name of Dictionary word, morpheme etc. if sound message, How? Think a later.
 	char entry;
-	//Meaning of syntax.
-	//Information about the relation of cooccurence.
-	//Information about the relation of replacement.
+	
+	//Information of syntax.
+	  //Information about the relation of replacement.
+	
+	    //Part of Speech(POS): 
+	    //POS value is No digit value, analogy value.
+	    //Noun, Verb, Adjective, Determiner, Adverb, Conjunction etc. I disagree postposition as POS in common korean grammar theory.
+	    //a port of POS is that the list is changing(noun, verb etc), the others is fix(determiner, conjunction etc). 
+	
+	
+	
+	  //Information about the relation of cooccurence.
+	
+	
 	
 	//Referential meaning
 	

cb78354ba798cfc740a3c06d6bf139cfd3f77fd1
iseonhwan <bollokrenzhaeanseon@gmail.com>
2021-11-30 07:14:53 +0900

    update memo

--- Reference/Perceptron_ex_2.py
+++ Reference/Perceptron_ex_2.py
@@ -1,18 +1,20 @@
-# define: Perceptron AND version example 1
+# define: Perceptron AND version example 2
 # date: 2021.11.14.
 # Resource: 밑바닥 인공지능(사이토코기)
 
 def AND(x1, x2):
-	w1, w2, theta = 0.5, 0.5, 0.7
-	tmp = x1*w1 + x2*w2
-	if tmp <= theta:
-		print('0')
+	x = np.array([x1 x2])
+	w = np.array([0.5, 0.5])
+	b = -0.7
+	tmp = np.sum(w*x) + b
+	if tmp <= 0:
 		return 0
-	elif tmp > theta:
-		print('1')
+	else:
 		return 1
 
 if __name__ == '__main__':
+	import numpy as np
+	
 	AND(0, 0)
 	AND(1, 0)
 	AND(0, 1)

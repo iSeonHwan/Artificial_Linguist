d6e7f1043193315ecdb5773340afb0d6bb332094
iseonhwan <84608929+iSeonHwan@users.noreply.github.com>
2021-12-28 21:08:14 +0900

    Create ReLU_2.cpp

---
+++ Reference/ReLU_2.cpp
@@ -0,0 +1,18 @@
+/*
+define: ReLU exapmle 2 cpp version
+date: 2021.12.29
+resource: 밑바닥 ~ 인공지능(사이코토기)
+*/
+
+double ReLU(double x){
+  if(x > 0){
+    return x
+  }
+  else if(x <= 0){
+    return 0
+  }
+  else{
+    
+  }
+}
+

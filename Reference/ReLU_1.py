bbda44c5892f894be2bcc03c2bc5334dd2016b3b
iseonhwan <bollokrenzhaeanseon@gmail.com>
2021-12-30 21:48:53 +0900

    maked ReLu code:

--- Reference/ReLU_1.py
+++ Reference/ReLU_1.py
@@ -1,10 +1,24 @@
 # definition: ReLU example version 1 py
 # date: 2021.12.29.
-# Resource: 밑바닥 인공지능(사이코토기)
+# Resource: 밑바닥부터 시작하는 인공지능(사이토고키, 2017)
 
 
 # relu function
 def relu(x):
    return np.maximum(0, x)
   
-  
+if __name__ == '__main__':
+   import numpy as np
+   import matplotlib.pylab as plt
+
+   ir1 = relu(-1)
+   ir2 = relu(0)
+   ir3 = relu(1)
+   ir4 = relu(2)
+
+   print(ir1)
+   print(ir2)
+   print(ir3)
+   print(ir4)
+
+   
--- Reference/ReLU_2.cpp
+++ Reference/ReLU_2.cpp
@@ -4,15 +4,34 @@ date: 2021.12.29
 resource: 밑바닥 ~ 인공지능(사이코토기)
 */
 
+#include <stdio.h>
+
+double ReLU(double x);
+
+int main(void){
+
+  int ir1 = ReLU(-1);
+  int ir2 = ReLU(0);
+  int ir3 = ReLU(1);
+  int ir4 = ReLU(2);
+
+  printf("%d\n", ir1);
+  printf("%d\n", ir2);
+  printf("%d\n", ir3);
+  printf("%d\n", ir4);
+
+  return 0;
+}
+
 double ReLU(double x){
   if(x > 0){
-    return x
+    return x;
   }
   else if(x <= 0){
-    return 0
+    return 0;
   }
   else{
     
   }
+  return -1;
 }
-

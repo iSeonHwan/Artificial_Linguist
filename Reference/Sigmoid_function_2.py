39723a8f6d6ce5bba811c14f84af06da9f542843
iseonhwan <bollokrenzhaeanseon@gmail.com>
2022-01-11 21:03:42 +0900

    study Neural Network, python

---
+++ Reference/Sigmoid_function_2.py
@@ -0,0 +1,18 @@
+# define: Sigmoid function version 1
+# date: 2021.12.23.
+# Resource: 밑바닥부터 시작하는 인공지능(사이토고키, 2017)
+
+def sigmoid(x):
+	import numpy as np
+	return 1 / (1+ np.exp(-x)) # exp is function to caculate the exponential of all elements in the input array. 
+
+if __name__ == '__main__':
+	import matplotlib.pylab as plt
+	
+	x = np.arange(-5.0, 5.0, 0.1)
+	y = sigmoid(x)
+
+	plt.plot(x, y)
+	plt.ylim(-0.1, 1.1)
+	plt.show()
+

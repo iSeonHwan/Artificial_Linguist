10d973de56483c64bf0dcde9052f5af74bff88dd
iseonhwan <bollokrenzhaeanseon@gmail.com>
2022-04-28 21:41:02 +0900

    study Array of cpp

---
+++ Reference/ArrayOfCPP_4.cpp
@@ -0,0 +1,29 @@
+#include <array>
+#include <iostream>
+
+typedef std::array<int, 4> Myarray;
+int main()
+{
+    typedef std::array<double, 4> Myarray;
+
+    Myarray c0 = { 0.5, 1.4, 2.3, 3.2 };
+
+    // display contents " 0 1 2 3"
+    for (const auto& it : c0)
+    {
+        //std::cout << " " << it;
+        std::cout << " "<< it;
+    }
+    std::cout << std::endl;
+
+    Myarray c1(c0);
+
+    // display contents " 0 1 2 3"
+    for (const auto& it : c1)
+    {
+        std::cout << " " << it;
+    }
+    std::cout << std::endl;
+
+    return (0);
+}
